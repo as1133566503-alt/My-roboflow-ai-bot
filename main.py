c1516502721d5292602067ddd441e53c19b125fa
as1133566503-alt <as1133566503@gmail.com>
2026-06-08 06:03:06 +0300

    fix bot

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram.ext import Application, MessageHandler, filters
 from telegram import Update
 from telegram.ext import ContextTypes
 
-TOKEN = 8798514063:AAHUIBaWbeBvIYRKAlvgoR0xVLj3in_oQqQ
+TOKEN = "8798514063:AAHUIBaWbeBvIYRKAlvgoR0xVLj3in_oQqQ"
 
 async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is running")
