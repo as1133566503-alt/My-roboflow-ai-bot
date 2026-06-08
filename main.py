7e547a06b893956c8b7161987d2d7e9967563341
as1133566503-alt <as1133566503@gmail.com>
2026-06-08 07:47:09 +0300

    Zz

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from telegram.ext import Application, MessageHandler, filters
-from telegram import Update
 from telegram.ext import ContextTypes
+from inference_sdk import InferenceHTTPClient
 
 TOKEN = "8798514063:AAHUIBaWbeBvIYRKAlvgoR0xVLj3in_oQqQ"
 
-async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🚗 تم استلام الصورة")
+CLIENT = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key="POJWbO5ntJbhoNjilxoE"
+POJWb
+    "
+)
+
+async def handle_photo(update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        await update.message.reply_text("جاري عد السيارات...")
+
+        file = await update.message.photo[-1].get_file()
+        await file.download_to_drive("image.jpg")
+
+        result = CLIENT.infer("image.jpg", model_id="cars-cars-cars/1")
+
+        predictions = result.get("predictions", [])
+        count = len(predictions)
+
+        await update.message.reply_text(f"🚗 عدد السيارات: {count}")
+
+    except Exception as e:
+        await update.message.reply_text(f"خطأ: {e}")
 
 app = Application.builder().token(TOKEN).build()
-
 app.add_handler(MessageHandler(filters.PHOTO, handle_photo))
 
 print("Bot Running...")
